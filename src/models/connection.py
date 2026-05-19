173509a1c440a4f28c26092a9466d423ef9b38c1
Kenia <23308060610371@cetis61.edu.mx>
2026-05-19 08:14:19 -0600

    xxx

--- src/models/connection.py
+++ src/models/connection.py
@@ -1,12 +1,9 @@
 import mysql.connector
 
 class Database:
-
     @staticmethod
     def get_connection():
-
         try:
-
             conn = mysql.connector.connect(
                 host="127.0.0.1",
                 user="root",
@@ -15,12 +12,9 @@ class Database:
                 port=3306
             )
 
-            if conn.is_connected():
-
-                print("Conexión a la BD exitosa")
-                return conn
+            print("Conexión a la BD exitosa")
+            return conn
 
         except mysql.connector.Error as err:
-
-            print(f"Error de conexión a MySQL: {err}")
-            return None
+            print("Error de conexión a MySQL:", err)
+            raise
